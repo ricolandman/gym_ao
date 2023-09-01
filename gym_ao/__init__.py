fddb2440b9d3ec7388b676f9b3e0bfa0acbb2f6d
Rico Landman <ricolandman@Ricos-MacBook-Pro.local>
2023-09-01 14:21:53 +0200

    Update init

---
+++ gym_ao/__init__.py
@@ -0,0 +1,9 @@
+from .gym_darkhole import *
+from .gym_sharpening import *
+
+__author__ = 'Rico Landman'
+__license__ = 'MIT'
+__version__ = '0.0.0'
+__maintainer__ = 'Rico Landman'
+__email__ = 'rlandman@strw.leidenuniv.nl'
+__status__ = 'Development'
